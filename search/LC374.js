d70655e285e76706d14e6849496ce795f0be308d
adam ye macbook <adamye001@gmail.com>
2023-06-04 22:26:49 +1000

    Add Leetcode questions and complete sort and search module

---
+++ search/LC374.js
@@ -0,0 +1,31 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let low = 1;
+  let high = n;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const res = guess(mid);
+    if (res === 0) {
+      return mid;
+    } else if (res === 1) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+};
+
+// Time Complexity: O(logN);
+// Space Complexity: O(1);
