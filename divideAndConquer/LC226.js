fc0c7af2f1ba0f1d7d2e6e92bb6ecda655c732d6
adam ye macbook <adamye001@gmail.com>
2023-06-05 15:14:52 +1000

    Complete divideAndConquer section, update code formatter config

---
+++ divideAndConquer/LC226.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (!root) {
+        return null;
+    }
+    return {
+        val: root.val,
+        left: invertTree(root.right),
+        right: invertTree(root.left),
+    };
+};
+
+// Time Complexity: O(n) (all nodes)
+// Space Complexity: O(h) (h => height of the binary tree)
--- divideAndConquer/LC374.js
+++ divideAndConquer/LC374.js
@@ -12,20 +12,22 @@
  * @return {number}
  */
 var guessNumber = function (n) {
-    let low = 1;
-    let high = n;
-    while (low <= high) {
+    const rec = (low, high) => {
+        if (low > high) {
+            return;
+        }
         const mid = Math.floor((low + high) / 2);
         const res = guess(mid);
         if (res === 0) {
             return mid;
         } else if (res === 1) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+            return rec(mid + 1, high);
+        } else if (res === -1) {
+            return rec(1, mid - 1);
         }
-    }
+    };
+    return rec(1, n);
 };
 
-// Time Complexity: O(logN);
-// Space Complexity: O(1);
+// Time Complexity: O(logN)
+// Space Complexity: O(logN)
