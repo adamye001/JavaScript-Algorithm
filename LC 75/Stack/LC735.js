2a6dfd3df4f6195529499a911bc00a6cd5df3a64
adam ye macbook <adamye001@gmail.com>
2023-06-13 15:47:17 +1000

    Complete Stack Module

---
+++ LC 75/Stack/LC735.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function (asteroids) {
+    const stack = [];
+    for (const aster of asteroids) {
+        let alive = true;
+        while (
+            alive &&
+            aster < 0 &&
+            stack.length > 0 &&
+            stack[stack.length - 1] > 0
+        ) {
+            alive = stack[stack.length - 1] < -aster;
+            if (stack[stack.length - 1] <= -aster) {
+                stack.pop();
+            }
+        }
+        if (alive) {
+            stack.push(aster);
+        }
+    }
+    return stack;
+};
+
+// Time Complexity: O(n);
+// Space Complexity: O(n);
